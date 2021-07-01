94c6b71d99beffa49a1ffc9c4103102ac9145c4a
Fill237 <andreeva.mariya94@bk.ru>
2021-07-01 17:00:01 +0300

    My first commit

---
+++ src/masha/Main.java
@@ -0,0 +1,17 @@
+package masha;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       int x = input.nextInt();
+       int x1 = input.nextInt();
+       int z = x + x1;
+       int v = x - x1;
+       System.out.println("Сложение: "+ x+ " + " +x1+ " " + "= " + z);
+       System.out.println("Вычитание: "+ x+ " - " +x1+ " " + "= " + v);
+
+    }
+}
